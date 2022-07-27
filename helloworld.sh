d901296e894e40b8841e0c300a309052c868d86a
Jon Salhus <salhusj96@gmail.com>
2022-07-27 01:17:07 -0400

    adds the helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
-# Author:                       
-# Date of latest revision:      
+# Author: Jon Salhus                  
+# Date of latest revision: 7/26/2022     
 # Purpose: Print a string to the terminal
 
 
